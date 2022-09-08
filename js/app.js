413e29151c259f97db4e305b9291f1a7886749c3
20181020076 <diegoarojas@hotmail.es>
2022-09-08 17:22:10 -0500

    aplicaciones creadas admin y empleado, funciona boton imprimir, delete y agregar

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 let usuarios=[{u:"ramon",p:"1234",e:"admin"},{u:"empleado1",p:"456",e:"empleado"}];
-let productos=[{nombre:"empanada",precio:2000},{nombre:"pastel",precio:2500}];
+let productos=[{nombre:"empanada",precio:2000,codigo:1},{nombre:"pastel",precio:2500,codigo:2}];
+let carrito = [];
 let nombres = usuarios.map(function(user){
     return user.u;
 });
@@ -38,15 +39,30 @@ function validar(){
     
 
 }
+function agregar(){
+    referencia = document.getElementById("codigo");
+    console.log(referencia);
+    productos.forEach((producto)=>{
+        if (referencia.value==producto.codigo){
+            carrito.push(producto)
+            panel.innerHTML += `<div class="aplicacion__panel__lista__item">${producto.nombre}, ${producto.precio}</div>`;
+        }
+    });
+}
 
 function inventario(){
+    carrito = [];
     panel.innerHTML = `<div class="aplicacion__panel__lista></div>`;
     productos.forEach((producto)=>{
         panel.innerHTML += `<div class="aplicacion__panel__lista__item">${producto.nombre}, ${producto.precio}</div>`
     });
 }
 function limpiar(){
+    carrito = [];
     panel.innerHTML = `<div class="aplicacion__panel__lista></div>`;
+}
+function imprimir(){
+    console.log(carrito);
 }
 function cerrar(){
     window.location.replace("/index.html");
